8a2fe6d53e15e22d1d280284ea413758187208e8
Mudasir <tadimarrimudasir@gmail.com>
2023-02-16 18:44:53 +0530

    variable names changed to cammel case, removed AutoViewInIt interface

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,19 +1,18 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.css']
 })
-export class EventComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    throw new Error('Method not implemented.');
-  }
+export class EventComponent {
   @ViewChild('barContainer', { static: false }) barContainerRef: ElementRef | undefined;
-  name: String = "smallllllllllllllllllllllllllllllllllllll";
-  slength:number=this.name.length;
+  name: String = "smalllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll";
+  nameLength:number=this.name.length;
+  oneHourPX:number=100;
+  oneCharPX:number=5;
+  paddingForEName:number=30;
   duration: number = 2;
-  bar_width: String = String(this.duration * 100 )+ "px";
-  container_width: String = String(Math.max((this.duration * 100),(this.slength*5+30)) )+ "px";
-
+  barWidth: String = String(this.duration * 100 )+ "px";
+  containerWidth: String = String(Math.max((this.duration * this.oneHourPX),(this.nameLength*this.oneCharPX+this.paddingForEName)) )+ "px";
 }
