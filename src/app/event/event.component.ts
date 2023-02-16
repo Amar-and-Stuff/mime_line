3eaea06bfceb56919dd0f93534561488470b1a49
Mudasir <tadimarrimudasir@gmail.com>
2023-02-16 18:20:53 +0530

    skeleton of event created

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,21 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.css']
 })
-export class EventComponent {
-  name: String = "longeeeeeeeeeeeest event name";
-  barlength = this.name.length;
-
-  getBarConatinerStyle() {
-    const barWidthPercent = 200;
-    return {
-      'bar-container': true,
-      'bar-container-width': true,
-      'bar-container-width-dynamic': true,
-      ['bar-container-width-dynamic--'+barWidthPercent]: true
-    };
+export class EventComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
   }
+  @ViewChild('barContainer', { static: false }) barContainerRef: ElementRef | undefined;
+  name: String = "smallllllllllllllllllllllllllllllllllllll";
+  slength:number=this.name.length;
+  duration: number = 2;
+  bar_width: String = String(this.duration * 100 )+ "px";
+  container_width: String = String(Math.max((this.duration * 100),(this.slength*5+30)) )+ "px";
+
 }
